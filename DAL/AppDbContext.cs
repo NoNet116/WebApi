bf55df3cb70fe0b09466b87bef992a3075661a50
Name <email@example.ru>
2025-07-09 16:14:20 +0300

    Createrd an etity for the tag / создал объект для тега

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -1,14 +1,25 @@
 ﻿using DAL.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace DAL
 {
     public class AppDbContext : IdentityDbContext<User>
     {
+        public DbSet<Tag> Tags { get; set; }
+
         public AppDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<AppDbContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            if (!Database.GetService<IRelationalDatabaseCreator>().Exists())
+            {
+                Database.EnsureCreated();
+            }
+            else
+            {
+                Database.Migrate();
+            }
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -34,5 +45,26 @@ namespace DAL
                     .IsUnique()
                     .HasDatabaseName("IX_User_Email");
             });
+
+            modelBuilder.Entity<Tag>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasIndex(e => e.Name).IsUnique();
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.CreatedAt)
+                    .HasDefaultValueSql("GETDATE()");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasDefaultValueSql("GETDATE()");
+            });
+
         }   }
 }
